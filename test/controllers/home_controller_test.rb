f792ad4c0a85821f6a90a2c9b734fec2ccb68b4a
am02785 <ar.matoorian@gmail.com>
2020-11-22 21:25:03 +0000

    Added testing

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
+# tests the home controller.
 class HomeControllerTest < ActionDispatch::IntegrationTest
+
+  # tests whether we can perform a get on home.
   test "should get home" do
-    get home_home_url
+    get root_url
     assert_response :success
+    
+    assert_select 'p', 'Welcome to the game central store.' # tests the content of the p element
   end
 
 end
