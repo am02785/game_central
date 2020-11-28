5473dbb789191dcece4198843f837999b380e69a
am02785 <ar.matoorian@gmail.com>
2020-11-28 20:45:46 +0000

    Added games title index

--- db/migrate/20201128184340_create_games.rb
+++ db/migrate/20201128184340_create_games.rb
@@ -1,11 +1,11 @@
 class CreateGames < ActiveRecord::Migration[5.2]
   def change
     create_table :games do |t|
-      t.string :title
-      t.text :description
-      t.string :image_url
-      t.decimal :price
-      t.date :release_date
+      t.string :title, null: false
+      t.text :description, null: false
+      t.string :image_url, null: false
+      t.decimal :price, null: false
+      t.date :release_date, null: false
 
       t.timestamps
     end
