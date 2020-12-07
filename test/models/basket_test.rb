434fbb9b36df843228156b15ec934ac81cd79ad6
am02785 <ar.matoorian@gmail.com>
2020-12-07 20:01:04 +0000

    Added test which tests whether a basket with a duplicate customer is not saved

--- test/models/basket_test.rb
+++ test/models/basket_test.rb
@@ -27,6 +27,21 @@ class BasketTest < ActiveSupport::TestCase
     assert basket.valid? # the basket should be saved
   end
 
+  # tests whether a basket with a duplicate customer cannot be saved
+  test 'should not save duplicate basket customer' do
+    basket1 = Basket.new # creates a new basket called basket1
+    basket1.total_cost = 49.99 # sets the total_cost of basket1
+    basket1.customer_id = @customer.id # sets the customer of basket1
+    basket1.save # attempts to save basket1
+    assert basket1.valid? # basket1 should be saved
+    
+    basket2 = Basket.new # creates a new basket called basket2
+    basket2.total_cost = 49.99 # sets the total_cost of basket2
+    basket2.customer_id = @customer.id # sets the customer of basket2 which is the same as the customer for basket1
+    basket2.save # attempts to save basket2
+    refute basket2.valid? # basket2 should not be saved
+  end
+
   # tests whether a basket with an invalid total_cost cannot be saved
   test 'should not save basket with invalid total_cost' do
     basket = Basket.new # creates a new basket
